63c251d50748b1e03d2f3f4426fc02f489ac7446
Maits27 <maitiurlo@gmail.com>
2023-03-18 13:51:51 +0100

    atributuen parametro ekorketa

--- src/fssInfoGain.java
+++ src/fssInfoGain.java
@@ -1,12 +1,15 @@
 import weka.attributeSelection.InfoGainAttributeEval;
 import weka.attributeSelection.Ranker;
 import weka.classifiers.Evaluation;
+import weka.classifiers.meta.AttributeSelectedClassifier;
+import weka.classifiers.meta.FilteredClassifier;
 import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.ConverterUtils;
 import weka.filters.Filter;
 import weka.filters.supervised.attribute.AttributeSelection;
+import weka.filters.supervised.instance.Resample;
 import weka.filters.unsupervised.attribute.Reorder;
 import weka.filters.unsupervised.attribute.StringToWordVector;
 import weka.filters.unsupervised.instance.Randomize;
@@ -17,7 +20,7 @@ import java.util.Random;
 public class fssInfoGain {
     public static void main(String[] args) {
         try {
-            args= new String[]{"trainBOW.arff", "hiztegiBerria.txt", " trainBOW_FSS_InfoGain.arff"};
+            args= new String[]{"trainBOW.arff", "hiztegiBerria.txt", "trainBOW_FSS_InfoGain.arff"};
             /** BOW/TFIDF --> InfoGain LORTU
              *  0. parametroa: .arff fitxategia (atributu guztiekin)
              *  1. parametroa: Hiztegi berria gordetzeko .txt
@@ -29,19 +32,32 @@ public class fssInfoGain {
             Instances data = source.getDataSet();
             data.setClassIndex(data.numAttributes() - 1);
 
-            //2. DATUAK RANDOMIZATU
-            Randomize r = new Randomize();
-            r.setSeed(42);
-            r.setInputFormat(data);
-            data=Filter.useFilter(data, r);
+            //2. DATUAK SEPARATU RESAMPLE BIDEZ
+            Resample resample = new Resample();
+            resample.setRandomSeed(42);
+            resample.setInvertSelection(false);
+            resample.setNoReplacement(true);
+            resample.setSampleSizePercent(66);
+            resample.setInputFormat(data);
+            Instances train = Filter.useFilter(data, resample);
+            train.setClassIndex(train.numAttributes()-1);
+
+            resample.setRandomSeed(42);
+            resample.setInvertSelection(true);
+            resample.setNoReplacement(true);
+            resample.setSampleSizePercent(66);
+            resample.setInputFormat(data);
+            Instances test = Filter.useFilter(data, resample);
+            test.setClassIndex(test.numAttributes()-1);
 
             //3. ATRIBUTUEN SELEKZIOA PLANTEATU:
             Ranker ranker = new Ranker();
             AttributeSelection as = new AttributeSelection();
             as.setEvaluator(new InfoGainAttributeEval());
             as.setSearch(ranker);
-            as.setInputFormat(data);
-            System.out.println(data.numAttributes());
+            as.setInputFormat(train);
+            System.out.println(train.numAttributes());
+
 
             int numaux=-1; //Numero de atributos que se quieren mantener (-1=todos)
             double taux=0.0; //Threshold puede ir del 0 (se mantienen todos los atributos) al 1 (se borran todos los atributos)
@@ -49,7 +65,7 @@ public class fssInfoGain {
 
             for(int n=0; n<data.numAttributes()-1; n+=10){
                 ranker.setNumToSelect(n);
-                for(double t=0.0; t<1.0; t+=0.1){
+                for(double t=0.0; t<1.01; t+=0.1){
                     ranker.setThreshold(t);
                     as.setSearch(ranker);
                     as.setInputFormat(data);
@@ -72,6 +88,7 @@ public class fssInfoGain {
                     "\nThreshold: "+taux);
             System.out.println("LORTU DEN F-MEASURE MAXIMOA:"+ fmax);
 
+
             ranker.setNumToSelect(numaux);
             ranker.setThreshold(taux);
             as.setSearch(ranker);
@@ -79,12 +96,8 @@ public class fssInfoGain {
             Instances filteredData= Filter.useFilter(data, as);
             filteredData.setClassIndex(filteredData.numAttributes()-1);
 
-            //4. HIZTEGIA GORDE
-            File hiztegia = new File(args[1]);
-            Instances trainBOWBerria= stringToWordVector(filteredData,hiztegia);
-
             //5. DATUAK GORDE
-            datuakGorde(args[2], trainBOWBerria);
+            datuakGorde(args[2], filteredData);
 
         }catch (Exception e){e.printStackTrace();}
     }
@@ -92,10 +105,8 @@ public class fssInfoGain {
     private static void datuakGorde(String path, Instances data) throws Exception {
 
         Reorder reorder = new Reorder();
-        reorder.setAttributeIndices("2-last,1");        //klasea azken atributua izateko
         reorder.setInputFormat(data);
         data = Filter.useFilter(data, reorder);
-
 
         ArffSaver s = new ArffSaver();
         s.setInstances(data);
@@ -103,17 +114,4 @@ public class fssInfoGain {
         s.writeBatch();
     }
 
-    private static Instances stringToWordVector(Instances train, File hiztegia) throws Exception {
-
-        StringToWordVector stringToWordVector= new StringToWordVector();
-        stringToWordVector.setLowerCaseTokens(true);                        // letra larria eta xeheak baliokideak
-        stringToWordVector.setIDFTransform(false);
-        stringToWordVector.setTFTransform(false);
-        stringToWordVector.setDictionaryFileToSaveTo(hiztegia);             // hiztegia gordetzeko fitxategia ezarri
-        //stringToWordVector.setWordsToKeep(2000);                            // bektorearen dimentsioa, coje las primeras x que tengan maiztasun mas grande ???
-        stringToWordVector.setOutputWordCounts(false);                      // bitarra
-        stringToWordVector.setInputFormat(train);
-        Instances trainBOW= Filter.useFilter(train,stringToWordVector);
-        return trainBOW;
-    }
 }
